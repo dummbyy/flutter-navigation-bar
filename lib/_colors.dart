565222ffb7acdd14b663c6c45277edc4ea74a355
Mert Karaca <38283518+sjuhyeon@users.noreply.github.com>
2021-08-12 11:25:12 +0300

    Delete _colors.dart

--- lib/_colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Primary_Color = Color(0xFF0C9869);
-const Text_Color = Color(0xFF3C4046);
-const Background_Color = Color(0xFFF9F8FD);
-
-const double Default_Padding = 20.0;
