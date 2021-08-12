90b5326939c1bfb1a6dcb43d59feb497e73f1238
Mert Karaca <38283518+sjuhyeon@users.noreply.github.com>
2021-08-12 10:21:51 +0300

    Update _colors.dart

--- lib/_colors.dart
+++ lib/_colors.dart
@@ -4,4 +4,4 @@ const Primary_Color = Color(0xFF0C9869);
 const Text_Color = Color(0xFF3C4046);
 const Background_Color = Color(0xFFF9F8FD);
 
-const double kDefaultPadding = 20.0;
+const double Default_Padding = 20.0;
