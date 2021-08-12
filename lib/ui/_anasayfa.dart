11dec57118b6abc4a16ebdc233b4a370146bfbfa
Mert Karaca <38283518+sjuhyeon@users.noreply.github.com>
2021-08-12 10:46:44 +0300

    Update _anasayfa.dart

--- lib/ui/_anasayfa.dart
+++ lib/ui/_anasayfa.dart
@@ -1,30 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:animox/page/_favoriler.dart';
+import 'package:animox/page/_hesabim.dart';
 
 class Anasayfa extends StatefulWidget {
-  Anasayfa({Key? key}) : super(key: key);
+  const Anasayfa({Key? key}) : super(key: key);
 
   @override
   _AnasayfaState createState() => _AnasayfaState();
 }
 
 class _AnasayfaState extends State<Anasayfa> {
+  final PageController _pageControl = PageController();
+  final List<Widget> _sayfalar = [
+    const Favoriler(),
+    const Hesabim(),
+  ];
+
+  void _onPageChanged(int indexOfPages) {}
+  void _onItemTapped(int selectedIndex) {
+    _pageControl.jumpToPage(selectedIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('Homepage')),
+      body: PageView(
+        controller: _pageControl,
+        children: _sayfalar,
+        onPageChanged: _onPageChanged,
+        physics: const NeverScrollableScrollPhysics(),
+      ),
       bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+        // ignore: prefer_const_literals_to_create_immutables
         items: [
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            title: Text('Home'),
+            label: 'Home',
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(Icons.favorite),
-            title: Text('Favorite'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text('account'),
-          ),
+            label: 'Favorite',
+          )
         ],
       ),
     );
