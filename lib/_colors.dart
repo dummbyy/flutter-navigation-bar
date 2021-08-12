2f149ca73a35e100de010d4965dcfd930d8cc709
Mert Karaca <38283518+sjuhyeon@users.noreply.github.com>
2021-08-12 10:21:39 +0300

    Update _colors.dart

--- lib/_colors.dart
+++ lib/_colors.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-// Colors that we use in our app
-const kPrimaryColor = Color(0xFF0C9869);
-const kTextColor = Color(0xFF3C4046);
-const kBackgroundColor = Color(0xFFF9F8FD);
+const Primary_Color = Color(0xFF0C9869);
+const Text_Color = Color(0xFF3C4046);
+const Background_Color = Color(0xFFF9F8FD);
 
 const double kDefaultPadding = 20.0;
